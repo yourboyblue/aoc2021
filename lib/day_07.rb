899f9529e9ce32a916a01f49e6d0d3373a708cb4
Ben Blue <bencblue@gmail.com>
2021-12-06 22:11:38 -0800

    Update naming for clarity

--- lib/day_07.rb
+++ lib/day_07.rb
@@ -1,22 +1,22 @@
 class Day07
   def initialize
-    @data = File.read('07.txt').split(',').map(&:to_i)
-    @positions = (@data.min..@data.max).to_a
+    @crabs = File.read('07.txt').split(',').map(&:to_i)
+    @positions = (@crabs.min..@crabs.max).to_a
   end
 
   def p2
-    puts @positions.map { |num| total_2(num) }.min
+    puts @positions.map { |pos| positional_alignment_cost_2(pos) }.min
   end
 
   def p1
-    puts @positions.map { |num| total(num) }.min
+    puts @positions.map { |pos| positional_alignment_cost(pos) }.min
   end
 
-  def total(num)
-    @data.sum { |pos| (num - pos).abs }
+  def positional_alignment_cost(pos)
+    @crabs.sum { |crab| (pos - crab).abs }
   end
 
-  def total_2(num)
-    @data.sum { |pos| (1..(num - pos).abs).sum }
+  def positional_alignment_cost_2(pos)
+    @crabs.sum { |crab| (1..(pos - crab).abs).sum }
   end
 end
