f56110e7342e81049197ff8ff4f0af0cba1aa0db
Ben Blue <bencblue@gmail.com>
2021-12-10 22:46:51 -0800

    rubocop

--- lib/day_11.rb
+++ lib/day_11.rb
@@ -1,4 +1,6 @@
-require_relative 'utils/grid'
+# frozen_string_literal: true
+
+require_relative "utils/grid"
 
 class Day11
   def initialize
@@ -28,20 +30,20 @@ class Day11
     flashed = Set[]
 
     @g.each { |x, y| @g.set(x, y, @g.val(x, y) + 1) }
-    to_flash = @g.select { |x, y| @g.val(x, y) > 9}
+    to_flash = @g.select { |x, y| @g.val(x, y) > 9 }
     step_flash_count = to_flash.length
 
-    while to_flash.any? do
+    while to_flash.any?
       new_to_flash = Set[]
       to_flash.each do |x, y|
-        @g.adj(x,y).each do |x1, y1|
-          if !flashed.include?([x1,y1])
-            @g.set(x1,y1, @g.val(x1,y1) + 1)
-            new_to_flash << [x1,y1] if @g.val(x1, y1) == 10
+        @g.adj(x, y).each do |x1, y1|
+          unless flashed.include?([x1, y1])
+            @g.set(x1, y1, @g.val(x1, y1) + 1)
+            new_to_flash << [x1, y1] if @g.val(x1, y1) == 10
           end
         end
-        @g.set(x,y,0)
-        flashed << [x,y]
+        @g.set(x, y, 0)
+        flashed << [x, y]
         @flash_count += 1
       end
       step_flash_count += new_to_flash.length
@@ -51,4 +53,3 @@ class Day11
     return true if step_flash_count == @octopus_count
   end
 end
-
